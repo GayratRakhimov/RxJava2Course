a7dda3c771b154c8506324f50a0ca9559e7cf365
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-05-16 20:33:34 +0500

    Simple example - Hello RxJava!

--- app/src/main/java/com/gayratrakhimov/rxjava2course/MainActivity.java
+++ app/src/main/java/com/gayratrakhimov/rxjava2course/MainActivity.java
@@ -1,7 +1,11 @@
 package com.gayratrakhimov.rxjava2course;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.just("Hello RxJava!")
+                .subscribe(s -> Log.d("RxJavaTag", "accept: "+s));
+
     }
 
 }
